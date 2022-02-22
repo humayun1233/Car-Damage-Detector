77ecb87a02780b30867db29ca643ebc74a7b549a
humayun1233 <87457794+humayun1233@users.noreply.github.com>
2022-02-22 15:43:24 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='Uploading', version='1.0',
       description='Code example demonstrating how to upload a file using Flask',
-      author='Ravi Ranjan', author_email='raviranjan.sjbit@gmail.com',
+      author='Humayun Mumtaz', author_email='mumtazhumayun6@gmail.com',
       url='https://github.com/raviranjan0309/Car-Damage-Detector.git',
 
       #  Uncomment one or more lines below in the install_requires section
